d7adad7f804f14ed6097938ace5fca7cc87f7b3f
pikodemo <pikodemo@71e836c5-6248-0410-81bd-4f409a761518>
2008-03-19 14:32:45 +0000

    Initial prototype import

---
+++ src/com/google/calculator/calc/InputTextView.java
@@ -0,0 +1,114 @@
+// Copyright 2008 Google Inc.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.calculator.calc;
+
+
+
+import com.google.calculator.evaluation.PredefinedFunctions;
+
+import java.util.Map;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+/**
+ * Input text view with autocompletion by word.
+ * 
+ * @author tulaurent@gmail.com (Laurent Tu)
+ */
+public class InputTextView extends AutoCompleteTextView {
+
+  private static final String[] kKeywords = PredefinedFunctions.getFunctionsName();
+  private int lastWordStartIndex;
+  private int lastCursorPosition;
+
+  public InputTextView(Context context) {
+    super(context);
+    setupAutoComplete();
+  }
+
+  public InputTextView(Context context, AttributeSet attrs, Map inflateParams) {
+    super(context, attrs, inflateParams);
+    setupAutoComplete();
+  }
+
+  public InputTextView(Context context, AttributeSet attrs, Map inflateParams,
+      int defStyle) {
+    super(context, attrs, inflateParams, defStyle);
+    setupAutoComplete();
+  }
+
+  /**
+   * Setup autocomplete.
+   */
+  protected void setupAutoComplete() {
+    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+        mContext, android.R.layout.simple_list_item_1, kKeywords);
+    setAdapter(arrayAdapter);
+    setThreshold(1);
+  }
+
+  protected void resetText() {
+    setText("", BufferType.EDITABLE);
+  }
+
+  @Override
+  protected void performFiltering(CharSequence text, int keyCode) {
+    // Detect the beginning of the last word in the line.
+    Log.i("filtering", "starting");
+    lastCursorPosition = getSelectionEnd();
+    lastWordStartIndex = 0;
+    for (int i = lastCursorPosition-1; i >= 0; i--) {
+      char c = text.charAt(i);
+      if (c == ' ' ||
+          c == '(' ||
+          c == ')' ||
+          c == '+' ||
+          c == '-' ||
+          c == '*' ||
+          c == '/' ||
+          c == '^') {
+        lastWordStartIndex = i+1;
+        break;
+      }
+    }
+    
+    // Extract last word.
+    CharSequence lastWord = null;
+    lastWord = text.subSequence(lastWordStartIndex, lastCursorPosition);
+    
+    // Perform filtering if last word length is bigger than filtering threshold.
+    if (lastWord.length() >= this.getThreshold()) {
+      super.performFiltering(lastWord, keyCode);
+    }
+  }
+
+  @Override
+  protected void replaceText(CharSequence text) {
+    CharSequence currentText = getText();
+    
+    // Replace currently auto-completed word.
+    CharSequence textKeptOnLeft = currentText.subSequence(0, lastWordStartIndex);
+    CharSequence textKeptOnRight = currentText.subSequence(lastCursorPosition, currentText.length());
+    super.replaceText(textKeptOnLeft + text.toString() + textKeptOnRight);
+    
+    // Set selection just after replaced word.
+    setSelection(lastWordStartIndex + text.length());
+  }
+
+}
